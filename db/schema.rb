d211839bd7ad93a2a745564660227f4189378a3f
tmccar5 <tommccarthyguitar@gmail.comssh-keygen -t rsa -C tommccarthyguitar@gmail.comcat>
2016-02-15 22:27:13 -0500

    prostgresql db

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160213191042) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "lessons", force: :cascade do |t|
     t.string   "date"
     t.text     "exercise"
@@ -26,7 +29,7 @@ ActiveRecord::Schema.define(version: 20160213191042) do
     t.datetime "chart_updated_at"
   end
 
-  add_index "lessons", ["user_id"], name: "index_lessons_on_user_id"
+  add_index "lessons", ["user_id"], name: "index_lessons_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -45,7 +48,8 @@ ActiveRecord::Schema.define(version: 20160213191042) do
     t.boolean  "admin",                  default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "lessons", "users"
 end
