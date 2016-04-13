31e01246e6d0784c7c3ad96e8acd6a72fa55c43e
tmccar5 <tommccarthyguitar@gmail.comssh-keygen -t rsa -C tommccarthyguitar@gmail.comcat>
2016-04-13 12:46:38 -0500

    calendar functionality

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,18 +1,21 @@
 class LessonsController < ApplicationController
-# before_action :find_lesson, only: [:show, :edit, :update, :destroy, :teacher]
 before_action :authenticate_user!
 
 	def index
 		@lessons = Lesson.where(:reciever_id => current_user.id).order(created_at: :desc)
-		@lesson = Lesson.new
 	end
 
 	def home
+		@lessons = Lesson.where(:reciever_id => current_user.id)
+		@lesson = @lessons.last
 	end
 
-	def teacher
-		# @user = User.find(params[:user_id])
-		@lessons = Lesson.all#where(['user_id=? OR receiver_id=?', @user.id, @user.id])
+	def teacher_cal
+		@lessons = Lesson.all
+	end
+
+	def student_cal
+		@lessons = Lesson.where(:reciever_id => current_user.id)
 	end
 
 	def new
@@ -24,21 +27,17 @@ before_action :authenticate_user!
 		@lesson = Lesson.new(lesson_params)
 		@lesson.user = current_user
 		@lesson.save
-	
 	end
 
 	def show
 		@lesson = Lesson.find(params[:id])
+		@user = User.find_by(:id => @lesson.reciever_id)
 	end
 
 private
 	
 	def lesson_params
-		params.require(:lesson).permit(:date, :exercise, :reciever_id, :chart, :start_time)
+		params.require(:lesson).permit(:title, :exercise, :reciever_id, :chart, :start_time)
 	end
-
-	# def find_lesson
-	# 	@lesson = Lesson.find(params[:id])
-	# end
 end
  
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412040023) do
+ActiveRecord::Schema.define(version: 20160413160947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20160412040023) do
   end
 
   create_table "lessons", force: :cascade do |t|
-    t.string   "date"
+    t.string   "title"
     t.text     "exercise"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
