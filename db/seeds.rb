8332828478dec397726464ab1ef8f0713d5fee90
tmccar5 <tommccarthyguitar@gmail.comssh-keygen -t rsa -C tommccarthyguitar@gmail.comcat>
2016-04-13 14:05:36 -0500

    added seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+user = User.new
+user.email = 'tom@last.com'
+user.password = 'Password320'
+user.password_confirmation = 'Password320'
+user.full_name = 'Tom Teacher'
+user.admin = true
+user.save!
+
+user2 = User.new
+user2.email = 'tom@testing.com'
+user2.password = 'Password320'
+user2.password_confirmation = 'Password320'
+user2.full_name = 'Tom Testing Student'
+user2.admin = false
+user2.save!
+
+
+
+
+
+
+puts "seed test"
